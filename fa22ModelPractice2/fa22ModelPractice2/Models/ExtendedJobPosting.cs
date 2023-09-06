ecee3aeb4c34d206e035d281977e329a3e84c64c
robertson-utexas <120421665+robertson-utexas@users.noreply.github.com>
2023-09-06 15:16:41 -0500

    commit

---
+++ fa22ModelPractice2/fa22ModelPractice2/Models/ExtendedJobPosting.cs
@@ -0,0 +1,26 @@
+﻿//add namespace for data annotations
+using System.ComponentModel.DataAnnotations;
+
+
+namespace fa22ModelPractice2.Models
+{
+    //TODO 01: Define an enum that will store the three work modalities
+    //the three modalities are in-person, remote, and hybrid
+
+
+    //TODO 02: Add the code so ExtendedJobPosting inherits from JobPosting
+    public class ExtendedJobPosting
+    {
+        //TODO 03: Add a property for the work modality of this posting
+        //the data type for this property MUST match the enum that
+        //you defined above
+
+
+        //TODO 04: Add a property for the sign-on bonus for this job posting
+        //TODO 05: Add the data annotations that will limit the range of the bonus
+        //sign on bonuses are between $0 and $10,000 
+
+
+        //TODO 06: Update the property for TotalAnnualCompensation
+    }
+}
